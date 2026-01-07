4f08c4e80fe9bbc3ce8ba79303d9872ecdce8110
Gejun Zhu <gejun.anderson@gmail.com>
2026-01-07 08:34:29 -0600

    allow access on local network

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    // Allow access from any device on your local network
+    'http://192.168.1.*',
+    // Or specify exact IPs if you prefer:
+    // 'http://192.168.1.195',
+  ],
 };
 
 export default nextConfig;
